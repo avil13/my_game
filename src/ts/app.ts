980fc5ca4e6d86a726b6377fd25bc8881bb1be68
avil13 <13.avil@gmail.com>
2016-04-05 00:24:39 +0300

    run

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,21 +2,61 @@
 
 module Game {
     export class ExtrimeWalking {
-        game: Phaser.Game;
+        game:Phaser.Game;
 
-        constructor(){
-            this.game =new Phaser.Game(800, 600, Phaser.AUTO, 'my-game', {
-               create: this.create,
+        private hero:any;
+        private background:any;
+
+
+        constructor() {
+            this.game = new Phaser.Game(500, 400, Phaser.AUTO, 'my-game', {
                 preload: this.preload,
-                update: this.update
+                create: this.create,
+                update: this.update,
+                render: this.render
             });
         }
 
-        create(){}
+        preload() {
+            this.background = this.game.load.image('background', '/img/background.png');
+            this.game.load.atlasJSONHash('hero', '/sprite/hero/hero.png', '/sprite/hero/hero.json');
+        }
 
-        preload(){}
+        create() {
+            var h = this.hero = this.game.add.sprite(100, 180, 'hero');
 
-        update(){}
+            h.animations.add('run', Phaser.Animation.generateFrameNames('hero/', 1, 21, '', 2), 10, true, false);
+            h.animations.play('run');
+
+            h.anchor.set(0.5, 0.5);
+            h.scale.setTo(1, 1);
+        }
+
+        update() {
+            var h = this.hero;
+
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                h.x -= 4;
+                if(h.scale.x < 0){
+                    h.scale.x *= -1;
+                }
+            } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                h.x += 4;
+                if(h.scale.x > 0){
+                    h.scale.x *= -1;
+                }
+            }
+
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+                h.y -= 4;
+            } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+                h.y += 4;
+            }
+        }
+
+        render() {
+            this.game.debug.spriteInfo(this.hero, 20, 32);
+        }
     }
 }
 
