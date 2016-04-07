0c1e889f0d76de399f089a32e7ff9991b896cccd
avil13 <13.avil@gmail.com>
2016-04-08 01:08:00 +0300

    a

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -33,15 +33,22 @@ module Game {
                 this.game.input.onDown.add(gofull, this);
             } //go fullscreen on mobile devices
 
-            // this.game.physics.startSystem(Phaser.Physics.P2JS); //activate physics
-            this.game.physics.startSystem(Phaser.Physics.NINJA);
+            this.game.physics.startSystem(Phaser.Physics.P2JS); //activate physics
+            this.game.physics.p2.gravity.y = 1200; //realistic gravity
+            this.game.world.setBounds(0, 0, 2000, 600); //(x, y, width, height)
+            this.game.physics.p2.setBoundsToWorld(true, true, false, true, false); //(left, right, top, bottom, setCollisionGroup)
+            this.game.physics.p2.friction = 5; // default friction between ground and player or fireballs
+
+            // this.clouds = this.game.add.tileSprite(0, 0, 2048, 600, 'clouds'); //add tiling sprite to cover the whole this.game world
+            this.ground = this.game.add.sprite(this.game.world.width / 2, this.game.world.height - 24, 'ground');
+            this.game.physics.p2.enable(this.ground); //enable physics so our player will not fall through ground but collide with it
+            this.ground.body.static = true; // ground should not move
+            this.game.physics.p2.friction = 5; // default friction between ground and player or fireballs
 
             this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
             // personages:
             this.hero = Hero.WhiteHero.create(this.game);
-
-            this.game.physics.ninja.enableAABB([this.hero]);
         }
 
         update() {
